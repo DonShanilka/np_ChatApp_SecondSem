8b10a049cc627f79819bc9e38aef8d9bb96ce723
Shanilka <shanilka799@gmail.com>
2024-01-16 14:35:14 +0530

    Client

--- src/main/java/lk/ijse/Controller/Server.java
+++ src/main/java/lk/ijse/Controller/Server.java
@@ -1,4 +1,16 @@
 package lk.ijse.Controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
+import javafx.event.ActionEvent;
+
 public class Server {
+
+    public JFXTextArea clientMsg;
+    public JFXTextArea viewSeverMsg;
+    public JFXButton send;
+
+    public void sendOnAction(ActionEvent actionEvent) {
+
+    }
 }
