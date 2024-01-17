71385ca319b88c530c4e9369d038297dd1a00bdb
Shanilka <shanilka799@gmail.com>
2024-01-17 23:36:53 +0530

    Login

--- src/main/java/lk/ijse/Controller/LoginController.java
+++ src/main/java/lk/ijse/Controller/LoginController.java
@@ -1,8 +1,10 @@
 package lk.ijse.Controller;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.scene.control.TextField;
 
 public class LoginController {
 
     public JFXButton loginOnAction;
+    public TextField txtPaassword;
 }
