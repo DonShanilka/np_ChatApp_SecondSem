b04b62580a068787ce82cf04fb3d9f10b39d87d0
Shanilka <shanilka799@gmail.com>
2024-01-22 23:06:03 +0530

    DbConnection

--- src/main/java/lk/ijse/db/DbConnection.java
+++ src/main/java/lk/ijse/db/DbConnection.java
@@ -1,5 +1,24 @@
 package lk.ijse.db;
 
-public class DbConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DbConnection {
+    private static DbConnection db;
+    private Connection connection;
+    private DbConnection() throws SQLException {
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/chat",
+                "root",
+                "Shanilka800@#"
+        );
+    }
+
+    public static DbConnection getInstance() throws SQLException {
+        return db == null ? db = new DbConnection() : db ;
+    }
+    public Connection getConnection(){
+        return connection;
+    }
 }
