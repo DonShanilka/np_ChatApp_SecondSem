fa41d6b54e84ce27db9a2b8bfb37a8461f8cdb1e
Shanilka <shanilka799@gmail.com>
2024-01-22 23:39:49 +0530

    login

--- src/main/java/lk/ijse/Model/UserModel.java
+++ src/main/java/lk/ijse/Model/UserModel.java
@@ -6,6 +6,7 @@ import lk.ijse.db.DbConnection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserModel {
@@ -22,4 +23,20 @@ public class UserModel {
         return pstm.executeUpdate() > 0;
     }
 
+    public boolean login(UserDto dto) throws SQLException {
+        Connection connection = DbConnection.getInstance().getConnection();
+        String sql = "SELECT * FROM user WHERE user_name = ? ";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1,dto.getName());
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            String pw = resultSet.getString(2);
+            if (pw.equals(dto.getPsw())){
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
