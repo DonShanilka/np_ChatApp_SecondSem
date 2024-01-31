f0e0991c104f827df4770594ab555f804a4775a1
Shanilka <shanilka799@gmail.com>
2024-01-31 11:39:52 +0530

    ClientController

---
+++ src/main/java/lk/ijse/Server/Client/Client.java
@@ -0,0 +1,133 @@
+/* Created By Sithira Roneth
+ * Date :1/29/24
+ * Time :13:05
+ * Project Name :Chat_Application
+ * */
+package lk.ijse.Server.Client;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import lk.ijse.Controller.ChatController;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.Socket;
+
+public class Client implements Runnable, Serializable {
+
+    private final String name;
+    private final Socket socket;
+    private final DataInputStream inputStream;
+    private final DataOutputStream outputStream;
+    private ChatController chatController;
+
+    public Client(String name) throws IOException {
+        this.name = name;
+
+        socket = new Socket("localhost", 3200);
+        inputStream = new DataInputStream(socket.getInputStream());
+        outputStream = new DataOutputStream(socket.getOutputStream());
+
+        outputStream.writeUTF(name);
+        outputStream.flush();
+        try {
+            loadScene();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void loadScene() throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/chatRoom.fxml"));
+        Parent parent = loader.load();
+        chatController = loader.getController();
+        chatController.setClient(this);
+        stage.setResizable(false);
+        stage.setScene(new Scene(parent));
+        stage.setTitle(name + "'s Chat");
+        stage.show();
+
+        stage.setOnCloseRequest(event->{
+            try {
+                inputStream.close();
+                outputStream.close();
+
+                socket.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+//        new Alert(Alert.AlertType.INFORMATION, "Server connected").show();
+        System.out.println("server connected for " + name);
+
+        stage.setOnCloseRequest(event -> {
+            try {
+                outputStream.writeUTF(" Has left! \uD83D\uDE13 ");
+                outputStream.flush();
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public void sendMessage(String msg) throws IOException {
+        outputStream.writeUTF(msg);
+        outputStream.flush();
+    }
+
+    public void sendImage(byte[] bytes) throws IOException {
+        //System.out.println(bytes);
+        outputStream.writeUTF("image");
+        outputStream.writeInt(bytes.length);
+        outputStream.write(bytes);
+        outputStream.flush();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void run() {
+        try {
+            outputStream.writeUTF("New Friend Joined to Chat");
+            outputStream.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        while (true) {
+            try {
+                String message = inputStream.readUTF();
+                if (message.equals("image")) {
+                    receiveImage();
+                } else {
+                    chatController.writeMessage(message);
+                }
+            } catch (IOException e) {
+                try {
+                    inputStream.close();
+                    outputStream.close();
+                    socket.close();
+                } catch (IOException ignored) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void receiveImage() throws IOException {
+        String utf = inputStream.readUTF();
+        int size = inputStream.readInt();
+        byte[] bytes = new byte[size];
+        inputStream.readFully(bytes);
+        chatController.setImage(bytes, utf);
+    }
+}
