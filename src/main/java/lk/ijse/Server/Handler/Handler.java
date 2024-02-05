7b8103fecad2a72ce2124a17b1af9ee51c0013d1
Shanilka <shanilka799@gmail.com>
2024-02-05 23:49:15 +0530

    delete unUse Code

--- src/main/java/lk/ijse/Server/Handler/Handler.java
+++ src/main/java/lk/ijse/Server/Handler/Handler.java
@@ -57,11 +57,6 @@ public class Handler implements Runnable {
         outputStream.flush();
     }
 
-    private void sendMessage(String sender, String msg) throws IOException {
-        outputStream.writeUTF(sender + ": " + msg);
-        outputStream.flush();
-    }
-
 
     private void receiveImage() throws IOException {
         int size = inputStream.readInt();
